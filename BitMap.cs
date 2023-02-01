78feb9c7dc2939dbc2394a3ff877d418362222c0
athomsAF <thomasderudder@sfr.fr>
2023-02-01 14:41:32 +0100

    ajout class bitmap

--- BitMap.cs
+++ BitMap.cs
@@ -7,6 +7,15 @@ namespace projects
 {
     public class BitMap
     {
+        public byte[] Header { get; set; }
+        public byte[] ImageInfo { get; set; }
+        public byte[] Image { get; set; }
 
+        public BitMap(string Path){
+            byte[] myfile = File.ReadAllBytes(Path);
+            this.Header= myfile.Where((x, i) => i < 14).ToArray();
+            this.ImageInfo= myfile.Where((x, i) => i >= 14 && i < 54).ToArray();
+            this.Image= myfile.Where((x, i) => i >= 54).ToArray();
+        }
     }
 }
